6eb040c8b64b19ef73adffcf113a1f692455ea52
David Price <thedavid@thedavidprice.com>
2022-11-15 08:03:29 -0800

    add goTrue Auth @latest

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,6 @@
+import GoTrue from 'gotrue-js'
+
+import { AuthProvider } from '@redwoodjs/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -7,12 +10,19 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+const goTrueClient = new GoTrue({
+  APIUrl: 'https://vermillion-otter-e00fa5.netlify.app/.netlify/identity',
+  setCookie: true,
+})
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <AuthProvider client={goTrueClient} type="goTrue">
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
